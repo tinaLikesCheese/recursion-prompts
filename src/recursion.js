7a0e97b792b339aa34efe16dae63dbca9f353c0c
Tina Le <tinale09@gmail.com>
2019-04-23 12:34:28 -0700

    completed factorial sumOfInt sumIntInArr ifEven

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,73 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if(n < 0) {
+		return null;
+	}
+
+	if(n === 0) {
+		return 1;
+	}
+
+	return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	var total = 0; 
+
+	if(array.length === 0){
+		return 0;
+	}
+
+	if(array.length === 1){
+		return total += array[0]
+	}
+
+	return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
+
+
 var arraySum = function(array) {
+	var total = 0; 
+
+	if(array.length === 0){
+		return 0;
+	}
+
+	if(array.length === 1 && !Array.isArray(array[0])){
+		return total += array[0];
+	} 
+
+	if(array.length === 1 && Array.isArray(array[0])){
+		return arraySum(array[0]);
+	}
+
+	if(typeof array[0] !== "number"){
+		return arraySum(array[0]) + arraySum(array.slice(1));
+	}
+
+	return array[0] + arraySum(array.slice(1));
 };
+
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	n = Math.abs(n);
+
+	if(n === 0) {
+		return true; 
+	}
+
+	if(n === 1) {
+		return false; 
+	}
+
+	return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
